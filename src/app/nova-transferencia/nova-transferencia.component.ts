93bb838bab357965ff78214f3cfafb3d5b5ef5a5
renanribs <renanrbrs@gmail.com>
2022-04-25 22:39:06 -0300

    Atualizando

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
   transferir() {
     console.log('Transferência solicitada!');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+
   }
 }
